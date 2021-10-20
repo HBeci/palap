0f5d032caf6929c1fef7feb81f93b54abe0343e7
HBeci <90601595+HBeci@users.noreply.github.com>
2021-10-20 14:12:50 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+szam1 = int(input('Mennyi az egyik szám? '))
+szam2 = int(input('Mennyi a másik szám? '))
+print('A két szám összege :',szam1 + szam2,)
+
+print('feladat2')
+
+print('A két szám különbsége :',szam1 - szam2,)
+
+print('feladat3')
+
+print('A két szám hányadosa :',szam1 / szam2,)
+
+print('feladat4')
+
+print('A két szám szorzata :',szam1 * szam2,)
